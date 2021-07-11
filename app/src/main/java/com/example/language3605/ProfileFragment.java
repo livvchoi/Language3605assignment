d2b0ba1a18a3657e85f91675327c8098522d4aa5
J0HNW00 <63095191+J0HNW00@users.noreply.github.com>
2021-07-11 18:57:41 +1000

    included sign out button in ProfileFragment

--- app/src/main/java/com/example/language3605/ProfileFragment.java
+++ app/src/main/java/com/example/language3605/ProfileFragment.java
@@ -1,15 +1,20 @@
 package com.example.language3605;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ProfileFragment extends Fragment {
+    private Button pSignOut;
 
     @Nullable
     @Override
@@ -17,6 +22,19 @@ public class ProfileFragment extends Fragment {
         View contentView = inflater.inflate(R.layout.fragment_profile, container, false);
 
         //code here
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        pSignOut = contentView.findViewById(R.id.btSignOut);
+        pSignOut.setText("Sign Out");
+        pSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (auth.getCurrentUser() != null)
+                    auth.signOut();
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return contentView;
     }

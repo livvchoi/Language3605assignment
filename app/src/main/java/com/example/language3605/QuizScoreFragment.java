f605c5a1b707a2c3de45d392bfd88ab1329d9c9b
J0HNW00 <63095191+J0HNW00@users.noreply.github.com>
2021-07-13 11:31:09 +1000

    added QuizScoreFragment xml and buttons

--- app/src/main/java/com/example/language3605/QuizScoreFragment.java
+++ app/src/main/java/com/example/language3605/QuizScoreFragment.java
@@ -4,30 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-public class AnswerQuizFragment extends Fragment {
-    private Button aqTestButton;
+public class QuizScoreFragment extends Fragment {
+    private TextView tvRetry;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View contentView = inflater.inflate(R.layout.fragment_answer_quiz, container, false);
+        View contentView = inflater.inflate(R.layout.fragment_quiz_score, container, false);
 
         //code here
-
-        //test switch to quizScore frag
-        aqTestButton = contentView.findViewById(R.id.answerQuizTestButton);
-        aqTestButton.setOnClickListener(new View.OnClickListener() {
+        tvRetry = contentView.findViewById(R.id.tvQuizScoreRetry);
+        tvRetry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.fragment_container, new QuizScoreFragment());
+                fragmentTransaction.replace(R.id.fragment_container, new AnswerQuizFragment());
                 fragmentTransaction.commit();
             }
         });

aab2714192b96a3410881071ce687b0a196bc5a7
J0HNW00 <63095191+J0HNW00@users.noreply.github.com>
2021-07-29 12:07:19 +1000

    created Profile class

---
+++ app/src/main/java/com/example/language3605/Profile.java
@@ -0,0 +1,223 @@
+package com.example.language3605;
+
+public class Profile {
+    private String Badge10LB;
+    private String Badge10LBImage;
+    private String Badge1LB;
+    private String Badge1LBImage;
+    private String Badge1Language;
+    private String Badge1LanguageImage;
+    private String Badge1Quiz;
+    private String Badge1QuizImage;
+    private String Badge20Quiz;
+    private String Badge20QuizImage;
+    private String Badge3Language;
+    private String Badge3LanguageImage;
+    private String Badge5Language;
+    private String Badge5LanguageImage;
+    private String Badge5Quiz;
+    private String Badge5QuizImage;
+    private String BadgeShare;
+    private String BadgeShareImage;
+    private int LanguageCount;
+    private int Progress;
+    private int QuestionCount;
+
+    public Profile(String badge10LB, String badge10LBImage, String badge1LB, String badge1LBImage,
+                   String badge1Language, String badge1LanguageImage, String badge1Quiz,
+                   String badge1QuizImage, String badge20Quiz, String badge20QuizImage,
+                   String badge3Language, String badge3LanguageImage, String badge5Language,
+                   String badge5LanguageImage, String badge5Quiz, String badge5QuizImage,
+                   String badgeShare, String badgeShareImage, int languageCount, int progress,
+                   int questionCount) {
+        Badge10LB = badge10LB;
+        Badge10LBImage = badge10LBImage;
+        Badge1LB = badge1LB;
+        Badge1LBImage = badge1LBImage;
+        Badge1Language = badge1Language;
+        Badge1LanguageImage = badge1LanguageImage;
+        Badge1Quiz = badge1Quiz;
+        Badge1QuizImage = badge1QuizImage;
+        Badge20Quiz = badge20Quiz;
+        Badge20QuizImage = badge20QuizImage;
+        Badge3Language = badge3Language;
+        Badge3LanguageImage = badge3LanguageImage;
+        Badge5Language = badge5Language;
+        Badge5LanguageImage = badge5LanguageImage;
+        Badge5Quiz = badge5Quiz;
+        Badge5QuizImage = badge5QuizImage;
+        BadgeShare = badgeShare;
+        BadgeShareImage = badgeShareImage;
+        LanguageCount = languageCount;
+        Progress = progress;
+        QuestionCount = questionCount;
+    }
+
+    public String getBadge10LB() {
+        return Badge10LB;
+    }
+
+    public void setBadge10LB(String badge10LB) {
+        Badge10LB = badge10LB;
+    }
+
+    public String getBadge10LBImage() {
+        return Badge10LBImage;
+    }
+
+    public void setBadge10LBImage(String badge10LBImage) {
+        Badge10LBImage = badge10LBImage;
+    }
+
+    public String getBadge1LB() {
+        return Badge1LB;
+    }
+
+    public void setBadge1LB(String badge1LB) {
+        Badge1LB = badge1LB;
+    }
+
+    public String getBadge1LBImage() {
+        return Badge1LBImage;
+    }
+
+    public void setBadge1LBImage(String badge1LBImage) {
+        Badge1LBImage = badge1LBImage;
+    }
+
+    public String getBadge1Language() {
+        return Badge1Language;
+    }
+
+    public void setBadge1Language(String badge1Language) {
+        Badge1Language = badge1Language;
+    }
+
+    public String getBadge1LanguageImage() {
+        return Badge1LanguageImage;
+    }
+
+    public void setBadge1LanguageImage(String badge1LanguageImage) {
+        Badge1LanguageImage = badge1LanguageImage;
+    }
+
+    public String getBadge1Quiz() {
+        return Badge1Quiz;
+    }
+
+    public void setBadge1Quiz(String badge1Quiz) {
+        Badge1Quiz = badge1Quiz;
+    }
+
+    public String getBadge1QuizImage() {
+        return Badge1QuizImage;
+    }
+
+    public void setBadge1QuizImage(String badge1QuizImage) {
+        Badge1QuizImage = badge1QuizImage;
+    }
+
+    public String getBadge20Quiz() {
+        return Badge20Quiz;
+    }
+
+    public void setBadge20Quiz(String badge20Quiz) {
+        Badge20Quiz = badge20Quiz;
+    }
+
+    public String getBadge20QuizImage() {
+        return Badge20QuizImage;
+    }
+
+    public void setBadge20QuizImage(String badge20QuizImage) {
+        Badge20QuizImage = badge20QuizImage;
+    }
+
+    public String getBadge3Language() {
+        return Badge3Language;
+    }
+
+    public void setBadge3Language(String badge3Language) {
+        Badge3Language = badge3Language;
+    }
+
+    public String getBadge3LanguageImage() {
+        return Badge3LanguageImage;
+    }
+
+    public void setBadge3LanguageImage(String badge3LanguageImage) {
+        Badge3LanguageImage = badge3LanguageImage;
+    }
+
+    public String getBadge5Language() {
+        return Badge5Language;
+    }
+
+    public void setBadge5Language(String badge5Language) {
+        Badge5Language = badge5Language;
+    }
+
+    public String getBadge5LanguageImage() {
+        return Badge5LanguageImage;
+    }
+
+    public void setBadge5LanguageImage(String badge5LanguageImage) {
+        Badge5LanguageImage = badge5LanguageImage;
+    }
+
+    public String getBadge5Quiz() {
+        return Badge5Quiz;
+    }
+
+    public void setBadge5Quiz(String badge5Quiz) {
+        Badge5Quiz = badge5Quiz;
+    }
+
+    public String getBadge5QuizImage() {
+        return Badge5QuizImage;
+    }
+
+    public void setBadge5QuizImage(String badge5QuizImage) {
+        Badge5QuizImage = badge5QuizImage;
+    }
+
+    public String getBadgeShare() {
+        return BadgeShare;
+    }
+
+    public void setBadgeShare(String badgeShare) {
+        BadgeShare = badgeShare;
+    }
+
+    public String getBadgeShareImage() {
+        return BadgeShareImage;
+    }
+
+    public void setBadgeShareImage(String badgeShareImage) {
+        BadgeShareImage = badgeShareImage;
+    }
+
+    public int getLanguageCount() {
+        return LanguageCount;
+    }
+
+    public void setLanguageCount(int languageCount) {
+        LanguageCount = languageCount;
+    }
+
+    public int getProgress() {
+        return Progress;
+    }
+
+    public void setProgress(int progress) {
+        Progress = progress;
+    }
+
+    public int getQuestionCount() {
+        return QuestionCount;
+    }
+
+    public void setQuestionCount(int questionCount) {
+        QuestionCount = questionCount;
+    }
+}
